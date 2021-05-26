702823f63f9646d58ba1f6927770a1f13533130a
Eduuard Pinzon <eduuardpinzon35@gmail.com>
2021-05-25 22:04:25 -0500

    se crean validaciones para products and bills

--- app/Http/Requests/BillStoreRequest.php
+++ app/Http/Requests/BillStoreRequest.php
@@ -24,7 +24,7 @@ class BillStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'client', => 'required',
+            'client' => 'required',
             'phone' => 'required',
             'email' => 'required',
         ];
