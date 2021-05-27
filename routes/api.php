dfbeac708d4d2d5cf5e59ecd572811673b1b497f
Eduuard Pinzon <eduuardpinzon35@gmail.com>
2021-05-26 22:24:20 -0500

    Configuración de rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BillController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,5 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
- Route::resource('product', ProductController::class);
+ Route::resource('products', ProductController::class);
+ Route::resource('bills', BillController::class);
