91927f0bc01a4c5deb63927877bd9df521de057b
Eduuard Pinzon <eduuardpinzon35@gmail.com>
2021-05-26 22:24:11 -0500

    migracion de tabla pivot de los productos seleccionados en cada factura

--- database/migrations/2021_05_26_012332_create_bills_table.php
+++ database/migrations/2021_05_26_012332_create_bills_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateBillsTable extends Migration
@@ -15,9 +16,7 @@ class CreateBillsTable extends Migration
     {
         Schema::create('bills', function (Blueprint $table) {
             $table->increments('id');
-
             //Numero de factura, Cliente, teléfono, email
-            $table->uuid('numBil');
             $table->string('client');
             $table->string('phone');
             $table->string('email');
